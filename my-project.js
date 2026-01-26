b7f6c445a5bbeb484faeae92ac490407199642a2
Sveta <artist.sv.chepurna@gmail.com>
2026-01-26 16:40:48 +0100

    add prompt into js file

--- my-project.js
+++ my-project.js
@@ -1,1 +1,8 @@
-console.log("It works!");
+
+let person = prompt("Please enter your name", "Clark Kent");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person;
+}
+
